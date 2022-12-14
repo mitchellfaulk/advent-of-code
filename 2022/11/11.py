aaa56b84d4b5264c436796f2812228e035c37f05
Mitchell Faulk <mitchellmfaulk@gmail.com>
2022-12-13 20:45:46 -0600

    change 11

--- 2022/11/11.py
+++ 2022/11/11.py
@@ -14,12 +14,8 @@ for line in f:
 			num = int(item.strip(','))
 			levels[count].append(num)
 		count += 1
-	
-
-
 
 # define monkey operations
-
 def do_op(item, idx):
 	if idx == 0:
 		return item * 11
@@ -38,24 +34,7 @@ def do_op(item, idx):
 	else: 
 		return item + 7
 
-# def do_op(item, idx):
-# 	if idx == 0:
-# 		return item * 19
-# 	elif idx == 1:
-# 		return item + 6
-# 	elif idx == 2:
-# 		return item * item
-# 	elif idx == 3: 
-# 		return item + 3
-
-
-d = {}
-for i in range(len(levels)):
-	d[i] = 0
-
-# print(d)
-
-
+# define tests
 def test(num, idx):
 	if idx == 0:
 		levels[7].append(num) if num % 2 == 0 else levels[4].append(num)
@@ -75,41 +54,55 @@ def test(num, idx):
 		levels[4].append(num) if num % 5 == 0 else levels[5].append(num)
 
 
-# def test(num, idx):
-# 	if idx == 0:
-# 		levels[2].append(num) if num % 23 == 0 else levels[3].append(num)
-# 	elif idx == 1:
-# 		levels[2].append(num) if num % 19 == 0 else levels[0].append(num)
-# 	elif idx == 2:
-# 		levels[1].append(num) if num % 13 == 0 else levels[3].append(num)
-# 	elif idx == 3:
-# 		levels[0].append(num) if num % 17 == 0 else levels[1].append(num)
-	
+# part 1
+
+d = {}
+for i in range(len(levels)):
+	d[i] = 0
 
 
-
-# for _ in range(20):
-# 	for i in range(len(d)):
-# 		# print(i, len(levels[i]))
-# 		d[i] += len(levels[i])
-# 		while levels[i]:
-# 			level = levels[i].pop()
-# 			# print(level)
-# 			level = do_op(level, i)
-# 			level = level//3
-# 			test(level,i)
-
-# print(d)
-
-# print(266 * 274)
-# 72884
-
-# # print(256*249)
-# # 63744 TOO LOW!
+for _ in range(20):
+	for i in range(len(d)):
+		# print(i, len(levels[i]))
+		d[i] += len(levels[i])
+		while levels[i]:
+			level = levels[i].pop()
+			# print(level)
+			level = do_op(level, i)
+			level = level //3
+			test(level,i)
 
 
-# print(2*3*5*7*11*13*17*19)
-# 9699690
+inspections = list(d.values())
+inspections.sort(reverse=True)
+print(inspections[0] * inspections[1])
+
+
+# part 2
+
+f = open('11.txt')
+
+# reset the levels list
+levels = []
+
+# read the data into the levels list
+count = 0 
+for line in f:
+	line = line.strip().split()
+	# print(line)
+	if line and line[0] == 'Starting':
+		levels.append([])
+		for item in line[2:]:
+			num = int(item.strip(','))
+			levels[count].append(num)
+		count += 1
+
+# reset the dictionary
+for i in range(len(levels)):
+	d[i] = 0
+
+# define the least common multiple of the primes appearing in the tests
+modulus = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
 
 for _ in range(10000):
 	for i in range(len(d)):
@@ -119,20 +112,16 @@ for _ in range(10000):
 			level = levels[i].pop()
 			# print(level)
 			level = do_op(level, i)
-			level = level % 9699690
+			level %= modulus
 			test(level,i)
 
-print(d)
+inspections = list(d.values())
+inspections.sort(reverse=True)
+print(inspections[0] * inspections[1])
 
-# print(120144*120141)
-# 14434220304 TOO LOW!
 
-# print(124006*124014)
-# 15378480084 TOO HIGH!
 
-# print(123741*123733)
 # 15310845153
-	
 	
 
 
